083c9513c755121a6c21117640b70caeec392bcd
Matt Oswald <moswald@gmail.com>
2015-12-03 16:42:22 -0600

    bare version for Day One

---
+++ AdventOfCode.Console/Console.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode.Console
+{
+    using System;
+    using AdventOfCode.Logic;
+
+    public static class Program
+    {
+        static void Main()
+        {
+            var input = "()";
+            var result = DayOne.GetFloor(input);
+            Console.WriteLine($"{result}");
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ AdventOfCode.Logic/DayOne.cs
@@ -0,0 +1,14 @@
+﻿namespace AdventOfCode.Logic
+{
+    using System.Linq;
+
+    public static class DayOne
+    {
+        public static int GetFloor(string directions)
+        {
+            return directions
+                .Select(ch => ch == ')' ? -1 : 1)
+                .Sum();
+        }
+    }
+}
