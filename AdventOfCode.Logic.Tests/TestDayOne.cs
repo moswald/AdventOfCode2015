7fbb269fad5bb29091e21f6456c2817ddf4be17e
Matt Oswald <moswald@gmail.com>
2015-12-08 14:05:02 -0600

    day one tests

---
+++ AdventOfCode.Logic.Tests/TestDayOne.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode.Logic.Tests
+{
+    using FluentAssertions;
+    using Xunit;
+
+    public class TestDayOne
+    {
+        [Theory]
+        [InlineData("(())", 0)]
+        [InlineData("()()", 0)]
+        [InlineData("(((", 3)]
+        [InlineData("(()(()(", 3)]
+        [InlineData("))(((((", 3)]
+        [InlineData("())", -1)]
+        [InlineData("))(", -1)]
+        [InlineData(")))", -3)]
+        [InlineData(")())())", -3)]
+        public void GetFloorReturnsCorrectResult(string directions, int expectedFloor)
+        {
+            DayOne.GetFloor(directions)
+                .Should().Be(expectedFloor);
+        }
+
+        [Theory]
+        [InlineData(")", 1)]
+        [InlineData("()())", 5)]
+        public void PositionOfBasementReturnsCorrectResult(string directions, int expectedPosition)
+        {
+            DayOne.PositionOfBasement(directions)
+                .Should().Be(expectedPosition);
+        }
+    }
+}
