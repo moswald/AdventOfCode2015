396abfc641730381a9f28bd210a1805facea3385
Matt Oswald <moswald@gmail.com>
2015-12-09 09:50:14 -0600

    namespace cleanup

--- AdventOfCode.Logic/DayThree.cs
+++ AdventOfCode.Logic/DayThree.cs
@@ -121,22 +121,3 @@
         }
     }
 }
-
-namespace AdventOfCode.Logic.Extensions
-{
-    using System.Collections.Generic;
-
-    static class ListExtensions
-    {
-        public static IEnumerable<T> Forever<T>(this IList<T> self)
-        {
-            for (;;)
-            {
-                for (var index = 0; index < self.Count; index++)
-                {
-                    yield return self[index];
-                }
-            }
-        }
-    }
-}
